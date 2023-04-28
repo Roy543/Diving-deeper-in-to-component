524b7d329761e9f9929ff6093ead172dbb6233fe
Roy543 <sahilsas88@gmail.com>
2023-04-28 19:05:41 +0200

    Dynamic Component

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,11 @@
         <h2>{{ slotProps.item }}</h2>
         <p>{{ slotProps['another'] }}</p>
     </course-goals>
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
+    <component :is="selectedComponent"></component>
   </div>
 </template>
 
@@ -19,16 +24,21 @@ import TheHeader from './components/TheHeader.vue';
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
 import CourseGoals from './components/CourseGoals.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
 
 export default {
   components:{
     TheHeader,
     BadgeList,
     UserInfo,
-    CourseGoals
+    CourseGoals,
+    ActiveGoals,
+    ManageGoals
   },
   data() {
     return {
+      selectedComponent: 'active-goals',
       activeUser: {
         name: 'Maximilian Schwarzmüller',
         description: 'Site owner and admin',
@@ -36,6 +46,11 @@ export default {
       },
     };
   },
+  methods:{
+    setSelectedComponent(cmp){
+      this.selectedComponent = cmp;
+    }
+  }
 };
 </script>
 
